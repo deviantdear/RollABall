95ea9d968f5879ca071f50c0939debde37622417
vadams4 <vadams4@asu.edu>
2019-01-30 12:20:28 -0700

    Added a little extra distance from player for better view

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,11 +7,12 @@ public class CameraController : MonoBehaviour
     public GameObject player;
 
     private Vector3 offset;
+    private Vector3 distanceFromBall = new Vector3 (0, 1.5f, 0);
 
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.transform.position;
+        offset = (transform.position - player.transform.position) + distanceFromBall;
     }
 
     // Late Update is called once per frame after all other items in the frame have loaded
